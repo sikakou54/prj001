2a925fc3da7cbb26ede097d24499e6aeb2d6a1d4
tally54 <sikakou0504@gmail.com>
2024-07-03 14:58:48 +0900

    commit

--- src/Compenent/ReceiveNotifyListItem.tsx
+++ src/Compenent/ReceiveNotifyListItem.tsx
@@ -42,7 +42,10 @@ export default function ReceiveNotifyListItem(props: Props) {
                         defaultIcon={<Text color={COLOR.WHITE}>{props.group_name.substring(0, 1)}</Text>}
                         size={45}
                     />
-                    <Box top={-8} right={1}>
+                    <Box
+                        top={-8}
+                        right={1}
+                    >
                         <AvatarIcon
                             img={props.img_user}
                             defaultIcon={<Text color={COLOR.WHITE}>{props.group_name.substring(0, 1)}</Text>}
